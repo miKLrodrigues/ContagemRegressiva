493473c036a613a93d7c60e7c674c72ef1f710c7
Mikaele Rodrigues <mikaelerodrigues07@outlook.com>
2025-09-19 22:08:38 -0300

    Contagem completa

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Pressione qualquer tecla para começar...");
+Console.ReadKey(true);
+
+Console.WriteLine("Preparar...");
+Console.ReadKey(true); 
+
+Console.WriteLine("Apontar...");
+Console.ReadKey(true);
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("FOGO!");
+Console.ResetColor();
